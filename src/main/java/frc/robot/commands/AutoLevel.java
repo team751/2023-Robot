c9f2d2171cfc3ea6e75804c462d7941e990b481d
Team751Lab <team751@gmail.com>
2023-03-30 16:18:53 -0700

    fixed bug in auto level

--- src/main/java/frc/robot/commands/AutoLevel.java
+++ src/main/java/frc/robot/commands/AutoLevel.java
@@ -1,14 +1,10 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.drivetrain.SwerveDrive;
-import frc.robot.subsystems.gyro.NavX2CompFilter;
-import frc.robot.subsystems.gyro.Odometry;
-
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.filter.Debouncer;
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
@@ -17,21 +13,14 @@ public class AutoLevel extends CommandBase{
     private AHRS navX2;
     private SwerveDrive swerveDrive;
     private PIDController levelXPIDController;
-    private SlewRateLimiter limiter;
     private Debouncer debouncer;
-    //private double ySpeed;
-
-    private boolean finished;
-    
+    //private double ySpeed;    
     public AutoLevel(AHRS navX2, SwerveDrive swerveDrive){
         this.navX2 = navX2;
         this.swerveDrive = swerveDrive;
         addRequirements(swerveDrive);
         this.debouncer = new Debouncer(3);
-        this.finished = false;
         levelXPIDController = new PIDController(0.6, 0, 0);
-        limiter = new SlewRateLimiter(1);
-        //ySpeed = 0;
     }
     
     @Override 
@@ -53,7 +42,9 @@ public class AutoLevel extends CommandBase{
 
     @Override
     public boolean isFinished(){
-        return debouncer.calculate(navX2.getPitch() < 6);
+        // Pitch in degrees
+        // TODO: Tune this threshold value
+        return debouncer.calculate(Math.abs(navX2.getPitch()) < 10);
     }
 
     @Override
